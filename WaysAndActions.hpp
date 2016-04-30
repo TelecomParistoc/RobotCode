3a6ed5734c55e3c83ddc32f2a3d57dfd76ea2475
Alkanoor <Alkanoor@github.com>
2016-04-30 19:32:07 +0200

    update, a tester

--- WaysAndActions.hpp
+++ WaysAndActions.hpp
@@ -1,16 +1,22 @@
+#include <pathfollower/pathFollower.hpp>
+#include <librobot/doors.h>
+#include <librobot/eater.h>
 #include "Action.hpp"
 
-/**Attention : ceci n'est qu'un exemple complètement irréaliste.
-* Chaque robot doit avoir son propre fichier ways_and_actions qui doit être rempli en conséquences
+/**
+*  This file maps the actions methods and paths to be executed by the main program
 **/
 
-std::vector<std::string> ways = {"move_cubes_go_fish","water2net","net2water","water2net","net2water","water2net","net2rocks","rocks2rocks"};
+std::vector<std::string> ways = {"start2cubes","zone2doors"};
 std::vector<Action> actions;
+std::pair<double,double> curPos = std::pair<double,double>(130,1255);
 
 void initWaysAndActions()
 {
-    actions.push_back(Action());
-    actions.push_back(Action(&pecher_start,&pecher_pause,&pecher_continue,&pecher_isFinished));
-    actions.push_back(Action(&relacher_start,&relacher_pause,&relacher_continue,&relacher_isFinished));
-    //etc ...
+    actions.push_back(Action()); // DO NOT REMOVE
+    actions.push_back(Action(startEaterAction, pauseEaterAction, resumeEaterAction, isEaterActionFinished));
+    actions.push_back(Action(startDoorsAction, pauseDoorsAction, resumeDoorsAction, isDoorsActionFinished));
+
+    PathFollower::setRadiusPositiveSpeed(180);
+    PathFollower::setRadiusNegativeSpeed(120);
 }
